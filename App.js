68aa8903e1336ba10c6ae969de586a88a852b933
Ayush PS <ayushpratap16@gmail.com>
2021-06-22 22:42:13 +0530

    Coordinate input
    Signed-off-by: Ayush PS <ayushpratap16@gmail.com>

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect,useState} from 'react';
 import Coords from './components/Coords'
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View ,KeyboardAvoidingView , TextInput, TouchableOpacity} from 'react-native';
 import * as Location from 'expo-location'
 
 export default function App() {
 
   const [curcor,setCor] =useState({lat:'',long:''}) 
+  const [coords,setLC] = useState([])
 
   useEffect(() => {
     load();
@@ -40,9 +41,22 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.Coords}>
       <Text style={styles.heading}>Your Coordinates</Text>
+       <Text style={styles.coords} >{curcor.lat}° , {curcor.long}°</Text>
         <Coords data={{name: 'Home',coord:curcor}}/>
         {/*Old Coordinates*/}
       </View>
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+                <TextInput style={styles.input} placeholder={'Add this location'}  onChangeText={text => setTask(text)} />
+        <TouchableOpacity >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -64,5 +78,48 @@ heading:{
   fontSize:24,
   fontWeight:'bold'
 
-}
+},
+coords:{
+  marginTop:40,
+  textAlign:'center',
+  fontSize:19,
+  fontWeight:'800',
+},
+writeTaskWrapper: {
+  position: 'absolute',
+  bottom: 60,
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'center'
+},
+input: {
+  paddingVertical: 15,
+  paddingHorizontal: 15,
+  backgroundColor: '#FFF',
+  borderRadius: 60,
+  borderColor: '#C0C0C0',
+  borderWidth: 1,
+  width: 250,
+},
+addWrapper: {
+  width: 60,
+  height: 60,
+  backgroundColor: 'dodgerblue',
+
+  borderWidth:0,
+  borderRadius: 60,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderColor: '#C0C0C0',
+  borderWidth: 1,
+  elevation:2,
+},
+addText: {
+  color:"white",
+  fontSize:26,
+  fontWeight:"500"
+},
+
+
 });
--- components/Coords.js
+++ components/Coords.js
@@ -1,16 +1,68 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function Coords({data}) {
-    const {name,coord:{lat,long}} = data
-    return (
-        <View>
-            <Text> {name} </Text>
-            <Text> {lat} {long} </Text>
+export default function Coords({ data }) {
+  const {
+    name,
+    coord: { lat, long },
+  } = data;
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <Text style={styles.Text}> {name} </Text>
+        
+        <Text style={styles.smallText}>
+          {lat}°, {long}°
+        </Text>
         </View>
-    )
+    
+      <View style={styles.circular}></View>
+    </View>
+  );
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
+  item: {
+      backgroundColor:"white",
+      padding:15,
+      borderRadius:10,
+      flexDirection:'row',
+      justifyContent:'space-between',
+      marginTop:30,  
+      elevation:4,
+      alignItems:'center',
+      shadowColor:"black",
+      shadowOpacity:0.8,
+      shadowRadius:50,
 
-})
+   
+
+  },
+ 
+  square: {
+    width:24,
+    height:24,
+    backgroundColor:'#55BCF6',
+    opacity: 0.4,
+    borderRadius:50
+
+  },
+  Text: {
+      fontSize:19,
+      fontWeight:"bold",
+      color:'dodgerblue',
+      paddingBottom:10,
+  },
+  smallText: {
+    fontSize:15,
+    fontWeight:"100",
+    color:'grey',
+  },
+  circular:{
+      width:12,
+      height:12,
+      borderColor:"dodgerblue",
+      borderWidth:2,
+      borderRadius:5,
+  },
+});
