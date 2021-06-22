dc958447ce2fb7cae8c1fcd1b1a78d5655aefa6c
Ayush PS <ayushpratap16@gmail.com>
2021-06-22 21:10:26 +0530

    Base
    Signed-off-by: Ayush PS <ayushpratap16@gmail.com>

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect,useState} from 'react';
+import Coords from './components/Coords'
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
 
 export default function App() {
+
+  const [curcor,setCor] =useState({lat:'',long:''}) 
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  const load = async () => {
+ 
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        seterr("Well the weatherman doesn't know where du are");
+        return;
+      }
+      const loc = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = loc.coords;
+      setCor({
+        lat:latitude,
+        long:longitude
+        
+      })
+
+    } catch (error) {
+      //Do something
+     }
+
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.Coords}>
+      <Text style={styles.heading}>Your Coordinates</Text>
+        <Coords data={{name: 'Home',coord:curcor}}/>
+        {/*Old Coordinates*/}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +51,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  Coords:{
+
+    paddingTop:80,
+    paddingHorizontal:20,
+
+
+  },
+heading:{
+  fontSize:24,
+  fontWeight:'bold'
+
+}
 });
